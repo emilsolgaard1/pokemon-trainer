22b722f61149846449cdd1a700711bb3b34a5b52
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-01-30 13:18:29 +0100

    Added environment variables: trainerApiUrl, trainerApiKey

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    trainerApiUrl: 'https://caramel-cypress-antelope.glitch.me/trainers/',
+    trainerApiKey: 'MyNZLtBYMvDEs7EsrsLn1fa2d1QTlEJswKHTUzka94Arf4Ctji9STEjXTEesAjra'
+  };
+  
