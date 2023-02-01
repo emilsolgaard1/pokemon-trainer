fd8498ab78445cad95f28d1d704416ae8db6cfc2
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-02-01 10:08:55 +0100

    added form validation to the login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -13,6 +13,8 @@ import { UserService } from 'src/app/services/user.service';
 export class LoginFormComponent {
  
   @Output() login: EventEmitter<void>=new EventEmitter();
+  
+   logingIn:boolean=false
  
   constructor( 
     private readonly loginService:LoginService,
@@ -21,12 +23,15 @@ export class LoginFormComponent {
 
   public loginSubmit(loginForm:NgForm):void{
 
+    this.logingIn=true
+
     const {username}=loginForm.value
-  
+    
     this.loginService.login(username)
     .subscribe({
       next:(user:User)=>{
         this.userService.user=user; 
+        this.logingIn=false
         this.login.emit()
       }, 
       error:()=>{}
