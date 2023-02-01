f59c52f0bd7a807298ddc874a29404b9e018323c
Farhang Younis <64683703+compsci-78@users.noreply.github.com>
2023-02-01 11:03:17 +0100

    removed environments from git

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    trainerApiUrl: 'https://caramel-cypress-antelope.glitch.me/trainers/',
-    pokemonsApiUrl:"https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
-    trainerApiKey: 'MyNZLtBYMvDEs7EsrsLn1fa2d1QTlEJswKHTUzka94Arf4Ctji9STEjXTEesAjra'
-  };
-  
