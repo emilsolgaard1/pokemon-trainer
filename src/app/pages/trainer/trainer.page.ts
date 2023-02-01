b41ef786dab996ec26d07ec5901bfd1cae0d677b
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-02-01 19:19:03 +0100

    Added trainer page functionality

--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-trainer',
@@ -6,4 +9,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./trainer.page.css']
 })
 
-export class TrainerPage {}
+export class TrainerPage {
+  get user(): User | undefined {
+    return this.userService.user
+  }
+
+  get collection(): Pokemon[]  {
+    if (this.userService.user) {
+      console.log(this.userService.user.pokemon)
+      return this.userService.user.pokemon
+    }
+
+    return []
+  }
+
+  constructor(
+    private readonly userService: UserService,
+  ) {
+
+  }
+}
